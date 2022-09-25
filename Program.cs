3cefe6ec142dab95a9a68fb063c71b95887c5c14
TimurIbatulin <mel0072007@icloud.com>
2022-09-25 10:24:37 +0300

    Третья задача

--- Program.cs
+++ Program.cs
@@ -28,25 +28,30 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.Write("Введите первую координату x -  ");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.Write("y - ");
-int y = Convert.ToInt32(Console.ReadLine());
-Console.Write("z");
-int z = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите вторую координату x1 -  ");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("y1 - ");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("z1");
-int z1 = Convert.ToInt32(Console.ReadLine());
-double l = Math.Sqrt((x-x1)*(x-x1)+(y-y1)*(y-y1)+(z-z1)*(z-z1));
-Console.Write($"Расстояние между первой и второй точкой состовляет {l}");
-
-
+// Console.Write("Введите первую координату x -  ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.Write("y - ");
+// int y = Convert.ToInt32(Console.ReadLine());
+// Console.Write("z");
+// int z = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите вторую координату x1 -  ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("y1 - ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("z1");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+// double l = Math.Sqrt((x-x1)*(x-x1)+(y-y1)*(y-y1)+(z-z1)*(z-z1));
+// Console.Write($"Расстояние между первой и второй точкой состовляет {l}");
 
 
 // Задача 23
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+Console.Write("Введите число N-");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i= 0; i <= N; i++)
+{
+    int z = i*i*i;
+    Console.WriteLine($"{i} в кубе равно {z}");
+}
